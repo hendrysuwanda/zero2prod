99bb43fddcaf797bd409431ec152dcb4fa6ac8e9
ms-suwanda.hendry <ms-suwanda.hendry@tech.jago.com>
2023-05-17 22:42:44 +0700

    add configuration & github action

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 use std::net::TcpListener;
+
 
 #[tokio::main]
 async fn main() -> std::io::Result<()>{
-    let listener = TcpListener::bind("127.0.0.1:8000")
+    //Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address)
         .expect("Failed to bind address");
     run(listener)?.await
 }
