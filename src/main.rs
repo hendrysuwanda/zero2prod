61b5aaf9f30be23f0e04314ba3efd8b1f5efc246
ms-suwanda.hendry <ms-suwanda.hendry@tech.jago.com>
2023-05-17 22:02:07 +0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use zero2prod::startup::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()>{
+    let listener = TcpListener::bind("127.0.0.1:8000")
+        .expect("Failed to bind address");
+    run(listener)?.await
+}
